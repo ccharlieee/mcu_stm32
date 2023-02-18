8230f0bc9a8c9016fd841995f3662bed02aeac7c
ccharlieee <812368191@qq.com>
2023-02-18 21:14:06 +0800

    integrate oled module

--- MDK-ARM/Objects/main.d
+++ MDK-ARM/Objects/main.d
@@ -35,3 +35,7 @@
 .\objects\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\misc.h
 .\objects\main.o: ..\USER\inc\dma.h
 .\objects\main.o: ..\USER\inc\uart_dma.h
+.\objects\main.o: ..\USER\inc\sys.h
+.\objects\main.o: ..\USER\inc\oled.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdlib.h
+.\objects\main.o: ..\USER\inc\bmp.h
